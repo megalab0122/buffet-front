c9858d4964b3ba9adc081016b471996cd570a7c2
Emirlan <admin@admins-MacBook-Pro.local>
2022-03-06 02:43:00 +0600

    added basket

--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Basket.css'
 const basket = require('../../assets/basket.png')
+const x = require('../../assets/Group.png')
 
 const Basket = () => {
+    const [ state, setState ] = useState<boolean>(true)
+
+
+
+    const toggleTrue = () => {
+        setState(true)
+    }
+
+    const toggleFalse = () => {
+        setState(false)
+    }
+
+    console.log(state)
+
     return (
-        <div>
-            <img src={basket} alt="" />
-        </div>
+        <>
+            <div>
+                <img src={basket} alt="" className='basket' onClick={toggleTrue}/>
+            </div>
+            {
+                state ?
+                    <div className='basketModal'>
+                        <div className='basketFon'>
+                            <div className='basketFonChild'>
+                                <img src={x} alt="" className='x' onClick={toggleFalse}/>
+                                <div>КОРЗИНА</div>
+                                <img src={basket} alt="" className='basket bas'/>
+                            </div>
+                        </div>
+                    </div>
+                   :
+                   null
+            }
+        </>
     );
 };
 
